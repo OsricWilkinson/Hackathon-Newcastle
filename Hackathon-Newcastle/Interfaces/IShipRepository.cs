250343584431c2a246dfd2faa2775db5f3482cfd
Paul Temperley <paul.temperley@hmrc.gsi.gov.uk>
2017-11-10 13:28:30 +0000

    Interfaces/basic Repos setup

---
+++ Hackathon-Newcastle/Interfaces/IShipRepository.cs
@@ -0,0 +1,18 @@
+﻿using Hackathon_Newcastle.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Hackathon_Newcastle.Interfaces
+{
+    public interface IShipRepository
+    {
+        IEnumerable<Ship> GetShip();
+        Ship GetShip(Guid shipId);
+        IEnumerable<Ship> GetShips(IEnumerable<Guid> shipId);
+        void AddShip(Ship ship);
+        void DeleteShip(Ship ship);
+        void UpdateShip(Ship ship);
+        bool ShipExists(Guid shipId);
+        bool Save();
+    }
+}
---
+++ Hackathon-Newcastle/Repositories/PlayerRepository.cs
@@ -0,0 +1,50 @@
+﻿using Hackathon_Newcastle.Interfaces;
+using Hackathon_Newcastle.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Hackathon_Newcastle.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        public void AddPlayer(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePlayer(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Player> GetPlayer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player GetPlayer(Guid playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Player> GetPlayers(IEnumerable<Guid> playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlayerExists(Guid playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePlayer(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
